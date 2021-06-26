486de6a0f539b55e379f195b44ca6293e19adf87
David Binetti <dbinetti@gmail.com>
2021-06-26 08:47:59 -0600

    Add admin test

--- project/app/tests/tests_admin.py
+++ project/app/tests/tests_admin.py
@@ -7,11 +7,11 @@ from django.urls import reverse
 def test_deploy():
     assert True
 
-# @pytest.mark.django_db
-# def test_index(admin_client):
-#     path = reverse('admin:index')
-#     response = admin_client.get(path)
-#     assert response.status_code == 200
+@pytest.mark.django_db
+def test_index(admin_client):
+    path = reverse('admin:index')
+    response = admin_client.get(path)
+    assert response.status_code == 200
 
 # @pytest.mark.django_db
 # def test_user(admin_client, user):
