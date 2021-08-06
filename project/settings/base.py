8e119356e518d4d97bb231de9c03cc7bf73cf15b
David Binetti <dbinetti@gmail.com>
2021-08-06 12:32:08 -0600

    linting

--- project/settings/base.py
+++ project/settings/base.py
@@ -199,7 +199,7 @@ TEMPLATES = [
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.template.context_processors.request',
-                'app.context_processors.avatar',
+                # 'app.context_processors.avatar',
             ],
         },
     },
