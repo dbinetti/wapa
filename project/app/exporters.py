5c84faaf8f053fec08b9f90b96c694593df5d1e1
David Binetti <dbinetti@gmail.com>
2022-06-09 10:22:13 -0600

    GeoJSON updaters

--- project/app/exporters.py
+++ project/app/exporters.py
@@ -2,12 +2,24 @@ import json
 
 
 def export_school(school):
+    boundary = json.loads(school.boundary.json)
     feature = {
         'type': 'Feature',
         'properties': {
             'name': school.name,
             'capacity': school.capacity,
         },
-        'geometry': school.boundary,
+        'geometry': boundary,
     }
-    return json.dumps(feature)
+    return feature
+
+def export_schools(schools):
+    features = []
+    for school in schools:
+        feature = export_school(school)
+        features.append(feature)
+    collection = {
+        'type': 'FeatureCollection',
+        'features': features,
+    }
+    return collection
